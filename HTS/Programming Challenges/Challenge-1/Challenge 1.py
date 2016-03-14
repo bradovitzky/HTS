3d9b0a073b1f46a7b10257c57880092f196119af
Benjamin Radovitzky <bradovitzky@isbos.org>
2016-03-14 11:47:55 -0400

    Update Challenge 1.py

--- HTS/Programming Challenges/Challenge-1/Challenge 1.py
+++ HTS/Programming Challenges/Challenge-1/Challenge 1.py
@@ -1,4 +1,3 @@
-lineNum=0
 Words=[]
 wordlist = open('wordlist.txt', 'r')
 for line in wordlist:
@@ -7,13 +6,14 @@ for line in wordlist:
         Word+=letter
         word = Word.replace("\n", "")
     Words.append(word)
-    lineNum+=1
 
 ScrambledList = open('Encrypted.txt', 'r')
 for cypher in ScrambledList:
     possibleWords = Words
     print(cypher)
+    matches = []
     for letter in cypher:
         print (letter)
-        match = [word for word in possibleWords if letter in word]
-    print (match)
+        matches.append[word for word in possibleWords if letter in word]
+    for match in matches:
+        if match in matches
